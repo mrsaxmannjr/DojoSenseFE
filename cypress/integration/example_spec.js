1167bf05532060e0fed975f8eaa64621a8127f1c
mrsaxmannjr <mrsaxmannjr@gmail.com>
2018-01-26 14:11:11 -0700

    towerApp

--- cypress/integration/example_spec.js
+++ cypress/integration/example_spec.js
@@ -1,6 +1,27 @@
 describe("Tower app", () => {
-  it("works", () => {
-    cy.visit("http://127.0.0.1:8080/");
-    cy.title().should("include", "Galvanize");  => the assert
+  it("has the correct Title", () => {
+    cy.visit("http://tower-fe.herokuapp.com/");
+    cy.title().should("include", "Dojo Sense");
+  });
+  it("Makes sure there are at least 5 styles listed", () => {
+    cy
+      .get(".dojo-div")
+      .find("div")
+      .should("have.length.least", 5);
+  });
+  it("Can list an individual style based on the style id and checks there is only one item displayed on the page", () => {
+    cy
+      .get("#listStyleForm")
+      .find("input")
+      .eq(0)
+      .type("2");
+    cy
+      .get("#listStyleForm")
+      .find("input")
+      .eq(1)
+      .click();
+    cy
+      .get(".dojo-div")
+      .should("have.length", 1);
   });
 });

eaa551a6a8f91206bca4f526649d05d5e82a7872
mrsaxmannjr <mrsaxmannjr@gmail.com>
2018-02-25 18:01:06 -0700

    api key added

--- src/Map.js
+++ src/Map.js
@@ -1,31 +1,30 @@
-import React, { Component } from 'react'
-import GoogleMapReact from 'google-map-react'
+import React, { Component } from "react";
+import GoogleMapReact from "google-map-react";
 
-const AnyReactComponent = ({ text }) => <div>{ text }</div>;
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 class Map extends Component {
   static defaultProps = {
     center: { lat: 40.195976, lng: -105.122658 },
-    zoom: 11
-  }
+    zoom: 11,
+  };
   render() {
     return (
-      <div className='google-map'>
+      <div className="google-map">
         <GoogleMapReact
           bootstrapURLKeys={{
             key: "AIzaSyB6IcDkdLFimq6E5YVNyuslp3Z2O2IpZU0",
           }}
-        resetBoundsOnResize
+          onGoogleApiLoaded={({ map, maps }) => console.log(map, maps)}
+          yesIWantToUseGoogleMapApiInternals
+          resetBoundsOnResize
           defaultCenter={{ lat: this.props.lat, lng: this.props.long }}
-          defaultZoom={ this.props.zoom }>
-          <AnyReactComponent
-            lat={ this.props.lat }
-            lng={ this.props.long }
-            text={ this.props.dojo }
-          />
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent lat={this.props.lat} lng={this.props.long} text={this.props.dojo} />
         </GoogleMapReact>
       </div>
-    )
+    );
   }
 }
 
